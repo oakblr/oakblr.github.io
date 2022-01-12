690647e04b2e318e911f00a18cde24935c988722
Aditya L <414B4C@gmail.com>
2022-01-13 00:49:23 +0530

    add countdown

---
+++ src/components/countdown.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react"
+import { Card, Stack, Typography } from "@mui/material"
+import siteData from "../../site-data"
+
+import glitchImg from "../images/glitch.jpg"
+
+function Countdown() {
+  const timeLeft = () => {
+    const t = Date.parse(siteData.codefestEndDate) - Date.parse(new Date())
+    return [
+      Math.floor(t / (1000 * 60 * 60 * 24)),
+      Math.floor((t / (1000 * 60 * 60)) % 24),
+      Math.floor((t / 1000 / 60) % 60),
+      Math.floor((t / 1000) % 60),
+    ]
+  }
+
+  const [countdown, setCountdown] = useState(timeLeft())
+
+  setInterval(() => setCountdown(timeLeft()), 1000)
+
+  return (
+    <div align="center">
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        justifyContent="center"
+        alignItems="center"
+        sx={{ maxWidth: "500px", width: "60%" }}
+      >
+        <div align="center">
+          <Card
+            sx={{
+              p: 1,
+              backgroundImage: `url(${glitchImg})`,
+              backgroundPosition: "100px 100px",
+            }}
+          >
+            <Typography variant="h3">{countdown[0]}</Typography>
+          </Card>
+          <p>Days</p>
+        </div>
+        <div align="center">
+          <Card
+            sx={{
+              p: 1,
+              backgroundImage: `url(${glitchImg})`,
+              backgroundPosition: "200px 100px",
+            }}
+          >
+            <Typography variant="h3">{countdown[1]}</Typography>
+          </Card>
+          <p>Hours</p>
+        </div>
+        <div align="center">
+          <Card
+            sx={{
+              p: 1,
+              backgroundImage: `url(${glitchImg})`,
+              backgroundPosition: "300px 100px",
+            }}
+          >
+            <Typography variant="h3">{countdown[2]}</Typography>
+          </Card>
+          <p>Minutes</p>
+        </div>
+        <div align="center">
+          <Card
+            sx={{
+              p: 1,
+              backgroundImage: `url(${glitchImg})`,
+              backgroundPosition: "400px 100px",
+            }}
+          >
+            <Typography variant="h3">{countdown[3]}</Typography>
+          </Card>
+          <p>Seconds</p>
+        </div>
+      </Stack>
+    </div>
+  )
+}
+
+export default Countdown
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import HomePageNavBar from "../components/homepage-navbar"
 import HomePageTypewriter from "../components/homepage-typewriter"
 import HomePageVideo from "../components/homepage-video"
+import Countdown from "../components/countdown"
 import { Button, Typography } from "@mui/material"
 
 const IndexPage = () => {
@@ -12,6 +13,8 @@ const IndexPage = () => {
       <main align="center">
         <HomePageVideo videoHeight={0.5} height={0.4} />
         <HomePageTypewriter />
+        <div style={{ margin: "20px" }}></div>
+        <Countdown />
         <Typography variant="h2">Hello World</Typography>
         <Button>Test</Button>
       </main>
