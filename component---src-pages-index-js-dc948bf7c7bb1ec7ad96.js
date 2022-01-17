d8d0c85d875bf70cec4edd5562babd907924b5a0
Aditya L <414B4C@gmail.com>
2022-01-17 16:27:55 +0530

    Updates

--- component---src-pages-index-js-dc948bf7c7bb1ec7ad96.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkoakridge_codefest=self.webpackChunkoakridge_codefest||[]).push([[678],{7704:function(e,t,n){n.r(t);var l=n(7294),a=n(5408),r=n(9989),c=n(6217);t.default=function(){return l.createElement(a.Z,null,l.createElement("main",{align:"center"},l.createElement(r.Z,{variant:"h2"},"Hello World"),l.createElement(c.Z,null,"Test")))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-dc948bf7c7bb1ec7ad96.js.map
