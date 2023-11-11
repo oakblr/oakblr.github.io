efc950e65cd3ad6d475698f8c50f481aafc1618d
Raviish Panicker <panicker.raviish2@gmail.com>
2023-11-12 00:25:38 +0530

    Updated animation for website and size

--- src/components/homepage-video.js
+++ src/components/homepage-video.js
@@ -4,34 +4,73 @@ import webmVideo from "../assets/cf-banner.webm"
 import mp4Video from "../assets/cf-banner.mp4"
 
 function HomePageVideo({ videoHeight, height }) {
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+      setTimeout(function() {
+        ref.current.style.opacity = "100%";
+      }, 1750);
+  })
+  
   return (
     <div
       style={{
         position: "relative",
         width: "100vw",
-        height: `${height * 100}vh`,
+        height: `${height * 100 + 30}vh`,
         zIndex: -1,
-        //background: `linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%)`,
+        // background: `linear-gradient(to left, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%)`,
       }}
     >
-      <video
+      <div
+        style={{
+          position: "relative",
+          width: "100vw",
+          height: `${videoHeight * 100}vh`,
+          zIndex: -1,
+          backgroundColor: "black",
+        }}
+      >
+        <video
         autoPlay
-        loop
         muted
         playsInline
         style={{
           position: "absolute",
           left: "50%",
           transform: "translateX(-50%)",
-          height: `${videoHeight * 100}vh`,
-          width: "100%",
-          objectFit: "cover",
+          height: `${videoHeight * 100 - 20}vh`,
+          width: "auto",
+          objectFit: "scale-down",
           zIndex: -1,
         }}
       >
         <source src={webmVideo} type="video/webm" />
         <source src={mp4Video} type="video/mp4" />
       </video>
+
+      <video
+        autoPlay
+        muted
+        playsInline
+        ref={ref}
+        style={{
+          opacity: "0%",
+          position: "absolute",
+          left: "50%",
+          transform: "translateX(-50%)",
+          height: `${videoHeight * 100 - 20}vh`,
+          width: "auto",
+          objectFit: "scale-down",
+          filter: "blur(16px)",
+          zIndex: -2,
+        }}
+      >
+        <source src={webmVideo} type="video/webm" />
+        <source src={mp4Video} type="video/mp4" />
+      </video>
+      </div>
+      
     </div>
   )
 }
