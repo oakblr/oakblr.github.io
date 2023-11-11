ddb23940fd713c59e5237486066d83dcf0452ffd
Raviish Panicker <panicker.raviish2@gmail.com>
2023-11-12 00:26:07 +0530

    Registrations message

--- src/site-data.js
+++ src/site-data.js
@@ -60,7 +60,7 @@ const siteData = {
   juniorSiteData: juniorSiteData,
   homepageSections: ["Themes", "Prizes", "FAQ", "Sponsors"],
   registerButton: {
-    text: "See you next year!",
+    text: "Registrations open soon!",
     url: "https://forms.gle/CJ3CEwH33nBvUvgeA",
     disabled: true,
   },
